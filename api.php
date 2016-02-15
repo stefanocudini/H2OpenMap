405651e8f457236748e6fae2800a79bd45290f44
sabas <sabas88@gmail.com>
2016-02-15 22:34:28 +0100

    Database table, query, map shows points based on tags

---
+++ api.php
@@ -0,0 +1,25 @@
+<?php
+
+function get_wells() {
+    $query = "SELECT * FROM h2openmap_field_data";
+    $db = new PDO('sqlite:h2openmap.db');
+    $res = $db->query($query);
+    $geoj=["type"=>"FeatureCollection","features"=>[]];
+    $ft=[];
+    while($r = $res->fetch( PDO::FETCH_ASSOC )){ 
+        $feat=["type"=>"Feature","properties"=>[],"geometry"=>["type"=>"Point", "coordinates"=>[]]];
+        $feat["geometry"]["coordinates"]=[$r['lon'],$r['lat']];
+        unset($r['lat']);
+        unset($r['lon']);
+        $feat["properties"]=$r;
+        $ft[]=$feat;
+    }
+    $geoj['features']=$ft;
+    $db = NULL;
+    return $geoj;
+}
+
+if(isset($_GET['wells'])) {
+    header('Content-type: application/json');
+    echo json_encode(get_wells(), JSON_NUMERIC_CHECK);
+}
